499b60b9312f66dc6f6da7be5dedbfddd3ebe2b4
akosvendler11 <91116457+akosvendler11@users.noreply.github.com>
2022-05-18 17:04:45 +0200

    Update cart.js

--- Főoldal/cart.js
+++ Főoldal/cart.js
@@ -1,169 +1,42 @@
-let carts = document.querySelectorAll('.btn');
+function kos() {
+    var list = "<tr><th style=\"width:450px; font-size: 40px;\">Termék </th><th style=\"width:450px; font-size: 40px;\">Mennyiség </th><th style=\"width:450px; font-size: 40px;\">Ár </th></tr>\n"
 
-let products = [
-    {
-        name: 'PlayStation 4 (PS4) Slim 500GB',
-        tag: 'ps4',
-        price: 100,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) Slim 500GB White',
-        tag: 'ps4w',
-        price: 110,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) Slim 1TB Spider-man Edition',
-        tag: 'ps4spider',
-        price: 135,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) Slim 1TB PlayStation Hits',
-        tag: 'ps4 1tb',
-        price: 150,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) pro 1TB',
-        tag: 'ps4pro',
-        price: 130,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) pro 1TB White',
-        tag: 'ps4 pro white',
-        price: 135,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) pro 1TB The Last Of Us Part 2 Edititon',
-        tag: 'ps4 last of',
-        price: 150,
-        inCart: 0
-    },
-    {
-        name: 'PlayStation 4 (PS4) pro 1TB Fifa 21 Edition',
-        tag: 'ps4 fifa',
-        price: 180,
-        inCart: 0
-    }
-];
+    var ossz = 0;
+    var menny = 0;
+    var key = "";
 
-for (let i=0; i < carts.length; i++)  {
-    carts[i].addEventListener('click', () => {
-        cartNumbers(products[i]);
-        totalCost(products[i])
-    })
-}
-function onLoadCartNumbers() {
-     let productNumbers = localStorage.getItem('cartNumbers'); 
-
-     if(productNumbers) {
-        document.querySelector('.cart span').textContent = productNumbers; 
-     }
-}
-
-function cartNumbers(product) {
     
-    let productNumbers = localStorage.getItem('cartNumbers');
-    
-    productNumbers = parseInt(productNumbers);
-
-    if( productNumbers ) {
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('.cart span').textContent = productNumbers + 1;
-    } else  {
-        localStorage.setItem('cartNumbers',   1);
-        document.querySelector('.cart span').textContent = 1;
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+        key = localStorage.key(i);
+        menny = Number(localStorage.getItem(key));
+        list += "<tr style=\"text-align: center; font-size: 20px;\"><td style=\"width:450px; font-size: 20px;\">" + key + "</td><td style=\"width:450px; font-size: 20px;\">" + "<button style:\"font-size: 20px;\" id=\"gombb\" onclick='ModifyItem(\"" + key + "\", " + "prompt(\"Írjon be egy összeget\")" + ")'>" + menny + "</button>" + "</td>" + "</td><td style=\"width:450px; font-size: 20px;\">" + (window[key] * menny) + " Ft" + "</td><td>" + "<button style=\"width:100%; font-size: 20px; border: none; color: red; background-color: white;\" onclick='töröl(\"" + key + "\")'>X</button>" + "</td></tr>";
+        ossz += window[key] * menny;
     }
 
-    setItems(product);
+    list += "<tr> <th>Fizetendő </th> <th> </th> <th>" + ossz + " Ft" + "</th></tr>"
+
+    document.getElementById('tabla').innerHTML = list
 }
 
-function setItems(product) {
-    let cartItems = localStorage.getItem('productsInCart');
-    cartItems = JSON.parse(cartItems);
+function termekek() {
+    var dbszam = 0;
 
-    if(cartItems != null) {
-        if(cartItems[product.tag] == undefined) {
-            cartItems = {
-                ...cartItems,
-                [product.tag]: product
-            }
-        }
-        cartItems[product.tag].inCart += 1;
-    } else {
-        product.inCart = 1;
-        cartItems = {
-            [product.tag]: product
-        }
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+        key = localStorage.key(i);
+        console.log(key)
+        dbszam += Number(localStorage.getItem(key))
     }
 
-    localStorage.setItem("productsInCart", JSON.stringify
-    (cartItems));
+    document.getElementById("darabszam").innerHTML = dbszam
+
 }
 
-function totalCost(product) {
-    //console.log("The product price is", product.price);
-    let cartCost = localStorage.getItem('totalCost');
-    
-    console.log("My cartCost is", cartCost)
-    console.log(typeof cartCost );
-
-    if(cartCost != null) {
-        cartCost = parseInt(cartCost);
-        localStorage.setItem("totalCost", cartCost + product.price);
-    } else {
-        localStorage.setItem("totalCost", product.price);
-
-    }
+function ModifyItem(key, darab) {
+    localStorage.setItem(key, Number(darab));
+    location.reload();
 }
 
-function displayCart() {
-    let cartItems = localStorage.getItem("productsInCart");
-    cartItems = JSON.parse(cartItems);
-    let productContainer = document.querySelector(".products");
-    let cartCost = localStorage.getItem('totalCost');
-    console.log(cartItems);
-
-    if( cartItems && productContainer ) {
-        productContainer.innerHTML = '';
-        Object.values(cartItems).map(item => {
-                productContainer.innerHTML += `
-                <div class="products">
-                <ion-icon name="close-circle"></ion-icon>
-                <img src="./images/${item.tag}.jpg">
-                <span>${item.name}</span>
-                </div>
-                <div class="price">${item.price},000 Ft</div>
-                <div class="quantity">
-                <ion-icon class="decrease "
-                name="arrow-dropleft-circle"></ion-icon>
-                <span>${item.inCart}</span>
-                <ion-icon class="increase"
-                name="arrow-dropright-circle"></ion-icon>
-                </div>
-                <div class="total">
-                       ${item.inCart * item.price},000 Ft
-                </div>
-                `;
-        });
-
-        productContainer.innerHTML += `
-            <div class="basketTotalContainer">
-                <h4 class="basketTotalTitle">
-                Összesen
-                </h4>
-                <h4 class=""basketTotal>
-                ${cartCost},000 Ft
-                </h4> 
-                </div>
-        `
-
-    }
+function töröl(key) {
+    localStorage.removeItem(key);
+    location.reload();
 }
-
-onLoadCartNumbers();
-displayCart();
